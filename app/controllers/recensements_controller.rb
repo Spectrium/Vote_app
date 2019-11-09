6c88d02a680362798eef4d39492499448a131658
S-Toky <stokyr29@gmail.com>
2019-11-09 19:55:00 +0300

    font list recensement

--- app/controllers/recensements_controller.rb
+++ app/controllers/recensements_controller.rb
@@ -4,11 +4,16 @@ class RecensementsController < ApplicationController
 	  @recensement = Recensement.all
 	  
 	  @q = @recensement.search(params[:q])
-  	@people = @q.result
+    @people = @q.result
   end
 
   def new
   	
+  end
+  
+  def show
+    
+ 
   end
 
   def create
@@ -42,8 +47,9 @@ class RecensementsController < ApplicationController
   end
 
   def destroy
-  	@recensement = Recensement.find(id: params[:id])
+  	@recensement = Recensement.find(params[:id])
     @recensement.destroy
+    redirect_to root_path
   end
 
 end
