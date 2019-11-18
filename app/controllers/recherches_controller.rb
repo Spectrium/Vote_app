62131394c2424aa7454660edd275acc69b0db2cb
Tsirimanana jean luc <tsiry.razanakoto@gmail.com>
2019-11-18 14:00:29 +0100

    resolve search

--- app/controllers/recherches_controller.rb
+++ app/controllers/recherches_controller.rb
@@ -1,7 +1,7 @@
 class RecherchesController < ApplicationController
 	def index
 		@params = params[:find]
-		@recensement = Recensement.where(['cin LIKE ?',"%#{@params}%"])
-		@recensement = Recensement.where(['full_name LIKE ?',"%#{@params}%"])
+		@recensement_cin = Recensement.where(['cin LIKE ?',"%#{@params}%"])
+		@recensement_name = Recensement.where(['full_name LIKE ?',"%#{@params}%"])
 	end
 end
